ea1acc3a9059f299267a9a14c4528112ca4a2c45
Thiago Zacarias Rufino <thiago_rufino@live.com>
2020-10-05 14:53:08 -0300

    Preparando ambiente pra recuperar e listar tarefas adicionadas

--- app_lista_tarefas/tarefa_controller.php
+++ app_lista_tarefas/tarefa_controller.php
@@ -4,14 +4,21 @@
     require "app_lista_tarefas/tarefa.service.php";
     require "app_lista_tarefas/conexao.php";
 
-    
-    $tarefa = new Tarefa();
-    $tarefa->__set('tarefa', $_POST['tarefa']);
 
-    $conexao = new Conexao();
-    
-    $tarefaService = new TarefaService($conexao, $tarefa);
-    $tarefaService->inserir();
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-    header('Location: nova_tarefa.php?inclusao=1')
+    if($acao == 'inserir') {
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa', $_POST['tarefa']);
+
+        $conexao = new Conexao();
+        
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->inserir();
+
+        header('Location: nova_tarefa.php?inclusao=1');
+    } else if($acao == 'recuperar') {
+        echo 'Chegamos até aqui!';
+    }
+
 ?>
